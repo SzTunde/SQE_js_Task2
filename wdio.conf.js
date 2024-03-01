20ce93c7c433188334c6d8ebd86dcb36d1b8b85d
SzTunde <37153591+SzTunde@users.noreply.github.com>
2024-03-01 06:15:00 +0100

    Update wdio.conf.js

--- wdio.conf.js
+++ wdio.conf.js
@@ -237,26 +237,26 @@ exports.config = {
      * @param {boolean} result.passed    true if test has passed, otherwise false
      * @param {object}  result.retries   information about spec related retries, e.g. `{ attempts: 0, limit: 0 }`
      */
-    afterTest: async (test, context, {
-        error,
-        result,
-        duration,
-        passed,
-        retries
-     }) => {
-        if (error) {
-            const fileName = test.title + ".png";
-            const dirPath = ".screenshots/";
+    //afterTest: async (test, context, {
+        //error,
+        //result,
+        //duration,
+        //passed,
+        //retries
+     //}) => {
+        //if (error) {
+            //const fileName = test.title + ".png";
+            //const dirPath = ".screenshots/";
 
-            if (!existsSync(dirPath)) {
-                mkdirSync(dirPath, {
-                    recursive: true,
-                });
-            }
+            //if (!existsSync(dirPath)) {
+                //mkdirSync(dirPath, {
+                    //recursive: true,
+                //});
+            //}
 
-            await browser.saveScreenshot(dirPath + fileName);
-        }
-     },
+            //await browser.saveScreenshot(dirPath + fileName);
+        //}
+     //},
 
 
     /**
